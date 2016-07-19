e7d6a26d879c1fd5da80cc6d9194014e1eb8ca3e
Andrey Pikelner <pikelner@theor.jinr.ru>
2016-07-19 18:10:23 +0200

    Mapping with exact number of legs working

--- mma/tools.m
+++ mma/tools.m
@@ -133,7 +133,8 @@ RewireX[po_] := MapIndexed[(x[#1] -> x[#2[[1]]])&, po];
 (* TODO:
    - Add possibility to map on exactly specified different masses and not only on signature
  *)
-ToposFromAux[ks_,legs_,prs_List] :=
+Options[ToposFromAux]={Verbose->False};
+ToposFromAux[ks_,legs_,prs_List,OptionsPattern[]] :=
     Module[{ds,ms,ufmap,scf,legSubsets,vertexCons},
            (* ds = First[#]^2& /@ prs; *)
 
@@ -174,13 +175,13 @@ ToposFromAux[ks_,legs_,prs_List] :=
                      (* Find polynomial ordering *)
                      po = First[PolynomialOrderings[up + fp, xs, 1]];
 
-                     Print["Poly order:",po];
+                     (* Print["Poly order:",po]; *)
                      (* Add numbering inside TOPO to propagators *)
                      (* STRANGE !!!!!!!! *)
                      MapIndexed[(newprops[[#2[[1]]]] = props[[#1]])&, po];
                      (* MapIndexed[(newprops[[#1]] = props[[#2[[1]]]])&, po]; *)
                      newprops = topo @@ newprops;
-                     Print["New props:", newprops];
+                     (* Print["New props:", newprops]; *)
 
                      xsub = RewireX[po];
                      (* Print["po= ",po, " props: ",props, "  ===  ", newprops, " xsub = ",xsub]; *)
@@ -193,7 +194,7 @@ ToposFromAux[ks_,legs_,prs_List] :=
                      (* Signature for mass distribution *)
                      msbin=List @@ (If[#[[3]]===0,0,1]& /@ newprops);
 
-                     Print["Mass sign ", msbin];
+                     (* Print["Mass sign ", msbin]; *)
                      (* ufprop=Association[upnew -> Association[monocrp -> Association[(If[#===0,0,1]& /@ ms) -> {crp,newprops}]]]; *)
 
                      (* Print["New rule: ",Association[upnew -> Association[monocrp -> Association[msbin -> {{crp,newprops}}]]]]; *)
@@ -209,7 +210,7 @@ ToposFromAux[ks_,legs_,prs_List] :=
                         
                         
                         kkU =ufmap[t][upnew];
-                        Print["kkU=",kkU];
+                        (* Print["kkU=",kkU]; *)
                         
                         If[MatchQ[kkU, Missing[_, _]],
                            (* New U value appending *)
@@ -236,7 +237,7 @@ ToposFromAux[ks_,legs_,prs_List] :=
                                  GetMasses[pr_]:= List @@ ((#[[3]])& /@ pr);
                                  mvec = GetMasses[newprops];
                                  
-                                 Print["M vetor ",mvec];
+                                 (* Print["M vetor ",mvec]; *)
                                  If[Length[Select[ufmap[[Key[t],Key[upnew],Key[monocrp],Key[msbin]]], mvec==GetMasses[#[[2]]]& ]] === 0,
                                     ufmap[[Key[t],Key[upnew],Key[monocrp],Key[msbin]]]=Append[ufmap[t][upnew][monocrp][msbin], {crp,newprops}]
                                 ]
@@ -278,27 +279,27 @@ ToposFromAux[ks_,legs_,prs_List] :=
            Do[
                Block[{topo=Head[topprops],lc,arestrict,aNot0,
                      solInt,solExt, exlst},
-                     Print["Conservation rules for topo ",topo];
+                     (* Print["Conservation rules for topo ",topo]; *)
                      lc=(Plus @@ Table[a[j]*topprops[[j,1]],{j,1,Length[topprops]}]);
                      (* We find linear combination with coefficients a={-1,0,1} only *)
                      arestrict=And @@ Table[(Abs[a[i]]==1||a[i]==0),{i,1,Length[topprops]}];
                      (* We do not interested in trivial solution when all a=0 *)
                      aNot0=(Plus@@Table[Abs[a[i]],{i,1,Length[topprops]}]) != 0;
 
-                     Print[lc];
+                     (* Print[lc]; *)
 
                      solInt=Solve[lc == 0 && And[arestrict] && aNot0,Table[a[j],{j,1,Length[topprops]}]];
-                     Print["IntRules:",solInt];
+                     (* Print["IntRules:",solInt]; *)
                      
                      exlst={};
                      (* Add internal rules {c1,c2,...}==0 *)
                      AppendTo[exlst,((Table[a[j],{j,1,Length[topprops]}]/.#)==0)]& /@ solInt;
 
                      Do[
-                         Print["Legs:",emom];
+                         (* Print["Legs:",emom]; *)
                          (* Total momentum flow through the cut is equal to external momentum *)
                          solExt=Solve[lc == (Plus@@emom) && And[arestrict] && aNot0,Table[a[j],{j,1,Length[topprops]}]];
-                         Print["Sol:",solExt];
+                         (* Print["Sol:",solExt]; *)
 
                          AppendTo[exlst,((Table[a[j],{j,1,Length[topprops]}]/.#)==Plus @@ emom)]& /@ solExt;
                          
@@ -313,15 +314,81 @@ ToposFromAux[ks_,legs_,prs_List] :=
           ]
 
 
+RemoveDotsUF[U_,F_]:=
+    Module[{X,mayHaveDots},
+           X = Union[Cases[U + F, x[_], -1]];
+           mayHaveDots=X;
+           AreHomo[poly_,x1_,x2_]:=FreeQ[Expand[poly/.x1->y-x2],x2];
+           
+           ReduceDotList[vars_,up_,fp_,sub_]:=
+           Module[{xTo,xFrom,HomoInx1x2,zzf,zzU,zzF,subret=sub,UpSub,FpSub},
+                  If[Length[vars] > 1,
+                     Print["Reduce:",vars];
+                     Print["U=",up];
+                     Print["F=",fp];
+                     Print["Sub=",subret];
+                     xTo=vars[[1]];
+                     xFrom=vars[[2;;-1]];
+                     {UpSub,FpSub,subret}=Catch[
+                         Do[
+                             HomoInx1x2=(AreHomo[up,xTo,xFrom[[i]]] && AreHomo[fp,xTo,xFrom[[i]]]);
+                             If[HomoInx1x2, 
+                                zzf=(z[xTo,xFrom[[i]]]/.x[a_]->a);
+                                zzU=up/.{xTo->zzf,xFrom[[i]]->0};
+                                zzF=fp/.{xTo->zzf,xFrom[[i]]->0};
+                                Print["Prepend: ",Prepend[Drop[xFrom,i],(z[xTo,xFrom[[i]]]/.x[a_]->a)]];
+                                {zzU,zzF,subret}=ReduceDotList[Prepend[Drop[xFrom,i],(z[xTo,xFrom[[i]]]/.x[a_]->a)],zzU,zzF,Append[subret,zzf->(xTo+xFrom[[i]])]];
+                                Throw[{zzU,zzF,subret}];
+                               ];
+                             ,{i,Length[xFrom]}
+                           ];
+                         Throw[{up,fp,subret}];
+                                               ]
+                     ,
+                     {UpSub,FpSub,subret}]
+                 ];
+           ReduceDotList[mayHaveDots,U,F,{}]
+          ]
 
-Options[MapOnAux]={ExactMatch->True};
-MapOnAux[ks_,legs_,prs_,{auxtop_,vertcons_},OptionsPattern[]] :=
-    Module[{up,pp,fp,xs,po,upnew,ppnew,fpnew,crp,monocrp,fkey,ds,ms,
-            possibleProps,nonZeroSymb,mappedWithMass,prsnew=prs},
+
+(* TODO *)
+(* 
+   - Removing dots
+   - Embeding topologies with dots instead of imaginary legs
+*)
+
+
+
+Options[MapOnAux]={
+    ExactMatch -> True,         (* Match only when all mass symbols are equal *)
+    RemoveDots -> True,         (* Remove dots from lines i.e. with eqaul momentum *)
+    Verbose    -> False};       (* Show more output *)
+
+MapOnAux[ks_,legs_,prsWdots_,{auxtop_,vertcons_},OptionsPattern[]] :=
+    Module[{prs,up,pp,fp,xs,po,upnew,ppnew,fpnew,crp,monocrp,fkey,ds,ms,
+            possibleProps,nonZeroSymb,mappedWithMass,prsnew},
            (* ds = First[#]^2& /@ prs; *)
            (* ms = Last[#]^2&  /@ prs; *)
-           {up,pp,fp,xs,ms} = UFP[ks,prs];
            
+           prs = If[OptionValue[RemoveDots],
+              Print["Removing dots before mapping"];
+              (* Print[RemoveDotsUF[up,fp]]; *)
+              
+              
+              Print["Eq: ",Tally[prsWdots, Expand[((#1[[1]])^2 - (#2[[1]])^2)] === 0&] ];
+              
+              First /@ Tally[prsWdots, Expand[((#1[[1]])^2 - (#2[[1]])^2)] === 0&]
+              
+              ,
+              Print["ATTENTION: dots not removed!!!"];
+              prsWdots
+             ];
+           
+           Print["PRS: ",prs];
+
+           prsnew = prs;
+           {up,pp,fp,xs,ms} = UFP[ks,prs];
+
            po = First[PolynomialOrderings[up + fp, xs, 1]];
            xsub = RewireX[po];
 
@@ -331,11 +398,11 @@ MapOnAux[ks_,legs_,prs_,{auxtop_,vertcons_},OptionsPattern[]] :=
            {upnew,ppnew,fpnew} = {up,pp,fp}/.xsub;
            crp      = CoefficientRules[ppnew,xs,DegreeReverseLexicographic];
            monocrp=(First /@ crp);
-           Print["U=",upnew];
-           Print["F=",fpnew];
+           (* Print["U=",upnew]; *)
+           (* Print["F=",fpnew]; *)
            
-           Print["Poly order:",po];
-           Print["prs:",prsnew];
+           (* Print["Poly order:",po]; *)
+           (* Print["prs:",prsnew]; *)
 
            topos=auxtop[[Key[Length[prsnew]],Key[upnew],Key[monocrp],Key[(If[#[[2]]===0,0,1]& /@ prsnew)]]];
 
@@ -347,7 +414,7 @@ MapOnAux[ks_,legs_,prs_,{auxtop_,vertcons_},OptionsPattern[]] :=
 
               (* Topology found in DB *)
               
-              Print["Mapping on top:",topos];
+              Print["Mapping on topo:",topos];
               (* topos[[]]; *)
               
               If[OptionValue[ExactMatch],
@@ -372,7 +439,7 @@ MapOnAux[ks_,legs_,prs_,{auxtop_,vertcons_},OptionsPattern[]] :=
                           vcrules = vertcons[top];
                           redvcr  = (Part[#[[1]],Flatten[auxpropnums]]==#[[2]])& /@ (Select[vcrules, (MatchQ[Union[Delete[#[[1]],auxpropnums]], {} | {0}])&]);
 
-                          Print["RCVCR ",redvcr];
+                          (* Print["RCVCR ",redvcr]; *)
                           subWrap = If[Length[legs] >= 2,(# -> wr[#])& /@ legs[[1;;-2]],{}];
                           
                           (* 
@@ -386,10 +453,10 @@ MapOnAux[ks_,legs_,prs_,{auxtop_,vertcons_},OptionsPattern[]] :=
                           pv = Transpose[{MapIndexed[(sgn[ #2[[1]] ] * #1[[1]])&, prsnew]}];
                           sys   = (First[#[[1]].pv/.subWrap]-#[[2]])& /@ redvcr;
                           syscr = Flatten[CoefficientRules[#,ks]& /@ sys];
-                          Print["Sys initial ",syscr];
+                          (* Print["Sys initial ",syscr]; *)
                           sgnsys    = And @@ ((#[[2]]==0)& /@ syscr);
-                          Print["ks: ",ks];
-                          Print["System ",sgnsys];
+                          (* Print["ks: ",ks]; *)
+                          (* Print["System ",sgnsys]; *)
                           sgnvars   = Table[sgn[i], {i,Length[prsnew]}];
                           sgnconstr = And @@ Table[Abs[sgn[i]]==1, {i,Length[prsnew]}];
                           (* Print["To solve ",{sgnsys && sgnconstr, Join[sgnvars,wr/@legs[[1;;-2]]]}]; *)
@@ -398,18 +465,26 @@ MapOnAux[ks_,legs_,prs_,{auxtop_,vertcons_},OptionsPattern[]] :=
                                       Solve[sgnsys && sgnconstr, Join[sgnvars,wr/@legs[[1;;-2]]]],
                                       Solve[sgnsys && sgnconstr, sgnvars]
                                      ];
-                          Print["All solutions",subsol];
+                          (* Print["All solutions",subsol]; *)
                           
                           (* First is with the smallest sum *)
                           plusSubSol=SortBy[subsol,(-Plus @@ (sgnvars/.#))&];
 
-                          Print["Most positive", First[plusSubSol]];
+                          (* Print["Most positive", First[plusSubSol]]; *)
 
+                          (* Decripting back *)
                           
-                          Print["GGGG" ];
-                          Print[pv];
-                          Print["Need to map on ", vertcons[top]];
+                          prsBack = prsnew;
+                          MapIndexed[(prsBack[[#1]] = (prsnew[[#2[[1]]]] -> 
+                                                       {p[extop[[2,#2[[1]],1]]]/sgn[#2[[1]]], (* Momenum in aux top *)
+                                                        extop[[2,#2[[1]],2]]/sgn[#2[[1]]],    (* Momentum *)
+                                                        extop[[2,#2[[1]],3]]                  (* Mass *)
+                                                       } ))&, po];
                           
+                          If[Length[legs] >= 2,
+                             {top @@ prsBack, wr/@legs[[1;;-2]]},
+                             {top @@ prsBack, {}}
+                            ]/.First[plusSubSol]
                          ]
                    ]
                  ,
@@ -420,70 +495,5 @@ MapOnAux[ks_,legs_,prs_,{auxtop_,vertcons_},OptionsPattern[]] :=
                 ]
               
              ]
-           
-           (* Catch[ *)
-
-           (*     Print["ppCR = ",monocrp]; *)
-           (*     GGG=Lookup[auxtop,upnew,Print["Topo with such U not found"];Throw[Null]]; *)
-           (*     Print["GGG",auxtop[[Key[upnew],Key[monocrp],Key[(If[#[[2]]===0,0,1]& /@ prs)]]]]; *)
-           (*     Print[Keys[GGG]]; *)
-           (*     lll=Lookup[ *)
-           (*         Lookup[auxtop,upnew,Print["Topo with such U not found"];Throw[Null]][[1]], *)
-           (*         monocrp,Print["No ppCR"];Throw[Null]]; *)
-           (*     Print["Found-----------------",lll]; *)
-                     
-           (*     Print["Orig CR ",First /@ ppCrules]; *)
-           (*     Print["Keys:",Keys[ft]]; *)
-           (*     Print[First/@(CoefficientRules[#,xs])&/@Keys[ft]]; *)
-
-           (*     (\* Select F-poly with the same set of monomials in x[i] *\) *)
-           (*     fkeys=Select[Keys[ft],(First/@(Sort[CoefficientRules[#,xs]])) == First/@ppCrules&]; *)
-
-           (*     If[Length[fkeys] == 0, *)
-           (*        Print["Topo with such F not found"]; *)
-           (*        Throw[Null], *)
-
-           (*        If[Length[fkeys] > 1, *)
-           (*           Print["Ambiguity in F poly identification"]; *)
-           (*           Throw[Null], *)
-
-           (*           (\* Only one equal monomial set *\) *)
-           (*           spSys = MapThread[(#1[[2]] == #2[[2]])&,{ppCrules,Sort[CoefficientRules[First[fkeys],xs]]}]; *)
-                     
-           (*           possibleProps=ft[First[fkeys]]; *)
-                     
-           (*           Print["Mass distr: ",(Last /@ #)& /@ possibleProps]; *)
-
-           (*           (\* symbol masses can not become zero after mapping *\) *)
-           (*           nonZeroSymb=And @@((# != 0)& /@ Union[Cases[ms,_Symbol]]); *)
-           (*           Print[Solve[(ms==#) && nonZeroSymb]& /@ ((Last /@ #)& /@ possibleProps)]; *)
-
-           (*           NonEmptyMass[pp_]:= Solve[(ms==(Last/@pp)) && nonZeroSymb]; *)
-
-           (*           Print["NEM:",NonEmptyMass /@ possibleProps]; *)
-
-           (*           (\* If mapping is possible system {m1,m2,m3,...}=={m4,m5,m6,...} has solution *\) *)
-           (*           (\* Equal mass mapped on equal mass, zero on zero etc. *\) *)
-           (*           mappedWithMass = Select[possibleProps, NonEmptyMass[#] =!= {}&]; *)
-                     
-           (*           If[Length[mappedWithMass]==0, *)
-           (*              Print["No mass distribution found"]; *)
-           (*              Throw[Null], *)
-                        
-           (*              If[Length[mappedWithMass] > 1, *)
-           (*                 Print["Mass distribution ambiguity"]; *)
-           (*                 Throw[Null], *)
-
-           (*                 (\* Mass distribution found *\) *)
-           (*                 Print[Solve[ (MapIndexed[sgn[#2[[1]]]*First[#1]&,prsnew]) == (First /@ First[mappedWithMass]), Table[sgn[j],{j,1,Length[prsnew]}]]]; *)
-           (*                 Throw[{spSys,upnew,First[fkeys],First[mappedWithMass]}] *)
-           (*                ] *)
-           (*             ] *)
-           (*           Print["Mass:  ",mappedWithMass]; *)
-
-           (*          ] *)
-           (*       ] *)
-           (*     Print["F match: ",fkeys]; *)
-           (*      ] *)
-
+          
           ]
